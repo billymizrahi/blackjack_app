4ba4b279f68a371c125852710f1eff8f8e26e9a8
Billy Mizrahi <billymizrahi@gmail.com>
2013-08-06 16:28:44 -0400

    added hit and stay functionality

--- blackjack_app.rb
+++ blackjack_app.rb
@@ -3,7 +3,7 @@ def calculate_total(cards)
 
 	total = 0
 	arr.each do |value|
-		if value = "A"
+		if value == "A"
 			total += 11
 		elsif value.to_i == 0
 			total += 10
@@ -17,6 +17,14 @@ def calculate_total(cards)
 	end
 
 	total
+end
+
+def hit(deck, mycards)
+	mycards << deck.pop
+end
+
+def dealer_hit(deck, dealercards)
+	dealercards << deck.pop
 end
 
 puts "Welcome to Blackjack!"
@@ -46,4 +54,29 @@ puts "Dealer has: #{dealercards[0]} and #{dealercards[1]}, for a total of #{deal
 puts "You have: #{mycards[0]} and #{mycards[1]}, for a total of #{mytotal}"
 puts ""
 puts "What would you like to do 1) hit 2) stay"
+
 hit_or_stay = gets.chomp
+
+if hit_or_stay == 1
+	hit(deck, mycards)
+	mytotal = calculate_total(mycards)
+
+	dealer_hit(deck, dealercards) if dealertotal < 17
+	dealertotal = calculate_total(dealercards)
+
+	puts "Dealer has a total of #{dealertotal}"
+	puts "You have a total of #{mytotal}"
+else
+	dealer_hit(deck, dealercards) if dealertotal < 17
+	dealertotal = calculate_total(dealercards)
+
+	puts "Dealer has a total of #{dealertotal}"
+	puts "You have a total of #{mytotal}"
+end
+
+
+
+
+
+
+
